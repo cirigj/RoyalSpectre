3811d5f2bffd2709f587354b07d66ac598977501
Bparker10 <parker4@rpi.edu>
2016-03-30 20:51:21 -0700

    Have environments set up, kitchen looks shitty and no hallways but whatevs. main menu has buttons to bring to each puzzle. 'M' goes back to main menu

---
+++ RoyalSpectre/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelHandler : MonoBehaviour {
+
+	public void GoToLevel(int lvl){
+		Application.LoadLevel (lvl);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.M))
+			Application.LoadLevel (0);
+	}
+}
