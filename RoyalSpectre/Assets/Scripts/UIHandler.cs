528cb83cafc3d8970ee2c61cefa2ed582cece2bb
Bparker10 <parker4@rpi.edu>
2016-04-27 19:41:03 -0700

    added pic to main menu, and added a canvas prefab with the health bar and skills bar. Needs to be hooked up becuase i cant push my scenes for reasons

---
+++ RoyalSpectre/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class UIHandler : MonoBehaviour, IPointerEnterHandler {
+
+	// Use this for initialization
+	void Start () {
+		GameObject skills = transform.GetChild (1).gameObject;
+		GameObject portrait = transform.GetChild (2).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerEnter(PointerEventData dataName)
+	{
+		//Debug.Log (gameObject.name);
+	}
+}
